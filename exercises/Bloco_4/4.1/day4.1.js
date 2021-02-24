04f00fd8441dd9df855bd8af56447aa40ab4a157
LuanAlexandre <luanrb19@gmail.com>
2021-02-24 18:19:37 -0400

    Dia 4.1 - JavaScript - Introdução e Primeiros Passos - Parte IV: Verifica e exibe a situação do candidato

--- exercises/Bloco_4/4.1/day4.1.js
+++ exercises/Bloco_4/4.1/day4.1.js
@@ -14,3 +14,10 @@ let perimetro = 2*base + 2*altura;
 
 //Parte IV
 let nota = 76;
+if (nota >= 80) {
+  console.log('Parabéns, você foi aprovado(a)')
+} else if (nota < 80 && nota >= 60) {
+  console.log('Você está na nossa lista de espera');
+} else {
+  console.log('Você foi reprovado(a)');
+}
