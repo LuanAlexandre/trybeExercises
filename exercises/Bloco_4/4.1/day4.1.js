5be47b6109f3b6245e2ff723db0dbb6ba821a8c2
LuanAlexandre <luanrb19@gmail.com>
2021-02-24 17:49:36 -0400

    Dia 4.1 - JavaScript - Introdução e Primeiros Passos - Parte III: Cria as variáveis base e altura

--- exercises/Bloco_4/4.1/day4.1.js
+++ exercises/Bloco_4/4.1/day4.1.js
@@ -1,3 +1,4 @@
+// Parte II
 const name = 'Luan Alexandre';
 const birthCity = 'Manaus';
 let birthYear = 1997;
@@ -5,3 +6,6 @@ console.log(name, birthCity, birthYear);
 birthYear = 2020;
 console.log(birthYear);
 birthCity = 'Canutama'; //constantes não podem ter seu valor alterado
+
+// Parte III
+let base = 5, altura = 10;
