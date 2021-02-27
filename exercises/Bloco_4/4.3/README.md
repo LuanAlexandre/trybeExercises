abd86537d70640b7b4eaa10f387987c4a338e54b
LuanAlexandre <luanrb19@gmail.com>
2021-02-27 16:00:57 -0400

    Logica Programaçao e Algoritmos: exercicio 4

--- exercises/Bloco_4/4.3/README.md
+++ exercises/Bloco_4/4.3/README.md
@@ -8,3 +8,4 @@ Exercício 2: Exibir um triângulo retângulo com asteriscos de base 5;
 
 Exercício 3: **_Inverter_** o triângulo do exercício 2;
 
+Exercício 4: Construir uma pirâmide com asteriscos de base **_n_**
---
+++ exercises/Bloco_4/4.3/exercicio4.js
@@ -0,0 +1,15 @@
+let n = 5;
+let asteriskPyramid;
+
+if (n > 1) {
+  for (indexLine = 0; indexLine < n; indexLine += 1) {
+    for (indexColumn = 0; indexColumn < n; indexColumn += 1) {
+      if (indexLine % 2 === 0) {
+        //linha 0 tem 1
+        //linha 2 tem 3
+        //linha 4 tem 5
+        
+      }
+    }
+  }
+}
