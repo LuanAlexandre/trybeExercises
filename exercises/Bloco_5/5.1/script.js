d65a10dba78bd564f1f9291e6a631a5de4435ab2
LuanAlexandre <luanrb19@gmail.com>
2021-03-05 20:49:51 -0400

    Dia 5.1 - JavaScript - DOM e Seletores: Exercicio.3

--- exercises/Bloco_5/5.1/script.js
+++ exercises/Bloco_5/5.1/script.js
@@ -1,3 +1,15 @@
-document.getElementsByTagName('p')[1].innerHTML = 'Empregado, Independencia financeira, Casa propria';
+function changeText(){
+  document.getElementsByTagName('p')[1].innerHTML = 'Empregado, Independencia financeira, Casa propria';
+}
 
-document.getElementsByClassName('main-content')[0].style.backgroundColor = 'rgb('+76+','+164+','+109+')';
+function changeColorGreen(){
+  document.getElementsByClassName('main-content')[0].style.backgroundColor = 'rgb('+76+','+164+','+109+')';
+}
+
+function changeColorWhite(){
+  document.getElementsByClassName('center-content')[0].style.backgroundColor = 'white';
+}
+
+changeText();
+changeColorGreen();
+changeColorWhite();
