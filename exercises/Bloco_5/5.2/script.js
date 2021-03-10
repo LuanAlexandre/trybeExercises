9cbace11cd4882be1eb6a9dec2c4ef9e7076f915
LuanAlexandre <luanrb19@gmail.com>
2021-03-09 22:21:03 -0400

    Dia 5.3 - Trabalhando com elementos - Pratica: Exercicio.2

--- exercises/Bloco_5/5.2/script.js
+++ exercises/Bloco_5/5.2/script.js
@@ -2,3 +2,5 @@ let title = document.createElement('h1');
 title.innerText = 'Exercicio 5.2 - JavaScript DOM';
 document.body.appendChild(title);
 
+let firstChildBody = document.createElement('div');
+document.body.appendChild(firstChildBody).className = 'main-content';
