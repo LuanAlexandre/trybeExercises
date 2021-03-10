47690c42740d5bbc4ebf7dfae8f8422bb73f29f1
LuanAlexandre <luanrb19@gmail.com>
2021-03-09 22:48:47 -0400

    Dia 5.3 - Trabalhando com elementos - Pratica: Exercicio.5

--- exercises/Bloco_5/5.2/script.js
+++ exercises/Bloco_5/5.2/script.js
@@ -1,12 +1,20 @@
+// Passo 1
 let title = document.createElement('h1');
 title.innerText = 'Exercicio 5.2 - JavaScript DOM';
 document.body.appendChild(title);
 
+// Passo 2
 let firstChildBody = document.createElement('div');
 document.body.appendChild(firstChildBody).className = 'main-content';
 
+// Passo 3
 let centerContent = document.createElement('div');
 document.querySelector('.main-content').appendChild(centerContent).className = 'center-content';
 
+// Passo 4
 let paragraph = document.createElement('p');
 document.querySelector('.center-content').appendChild(paragraph).innerText = 'Pizza Frita';
+
+// Passo 5
+let left = document.createElement('div');
+document.querySelector('.main-content').appendChild(left).className = 'left-content';
