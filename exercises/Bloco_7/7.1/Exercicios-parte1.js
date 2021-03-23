75b4dcdd835cab1ebf4a9c3e53ee42feec83700d
LuanAlexandre <luanrb19@gmail.com>
2021-03-23 18:39:49 -0400

    Dia 7.1 - JavaScript ES6 - let, const, arrow functions e template literals: Exercicio Pt.1 v2

--- exercises/Bloco_7/7.1/Exercicios-parte1.js
+++ exercises/Bloco_7/7.1/Exercicios-parte1.js
@@ -1,12 +1,8 @@
-function testingScope(escopo) {
-  if (escopo === true) {
-    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
-    console.log(ifScope);
-  } else {
-    let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-    console.log(elseScope);
-  }
-}
+let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+
+const testingScope = (escopo) => ( escopo ?
+  ifScope = ifScope + ' ótimo, fui utilizada no escopo !' && console.log(ifScope) : console.log(elseScope)
+);
 
 testingScope(true);
