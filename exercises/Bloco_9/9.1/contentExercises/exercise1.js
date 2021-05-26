920c15ffc97a973762869357da679e1c3408d04c
LuanAlexandre <luanrb19@gmail.com>
2021-05-26 17:26:52 -0400

    Dia 9.1 - JavaScript Assincrono e Callbacks: callbacks - solucao do ex1

--- exercises/Bloco_9/9.1/contentExercises/exercise1.js
+++ exercises/Bloco_9/9.1/contentExercises/exercise1.js
@@ -3,15 +3,16 @@ const assert = require('assert');
 const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
 const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
 
-const getUser = () => {
+const getUser = (callback) => {
   const userToReturn = {
     firstName: "Ivan",
     lastName: "Ivanovich",
     nationality: "Russian"
   };
+  return callback(userToReturn);
 };
 
 // complete a chamada da função de getUser
-assert.strictEqual(getUser(), "Hello! My name is Ivan Ivanovich");
+assert.strictEqual(getUser(userFullName), "Hello! My name is Ivan Ivanovich");
 // complete a chamada da função de getUser
-assert.strictEqual(getUser(), "Ivan is Russian");
+assert.strictEqual(getUser(userNationality), "Ivan is Russian");
