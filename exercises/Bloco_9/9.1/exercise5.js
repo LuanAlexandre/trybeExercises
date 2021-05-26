b2035c6b6082e53201906004f792c49c590181b0
LuanAlexandre <luanrb19@gmail.com>
2021-05-26 19:28:48 -0400

    Dia 9.1 - JavaScript Assincrono e Callbacks: solucao do ex 5

--- exercises/Bloco_9/9.1/exercise5.js
+++ exercises/Bloco_9/9.1/exercise5.js
@@ -10,7 +10,13 @@ const temperatureInFahrenheit = (temperature) => console.log(`It is currently ${
 const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
 
 // definição da função sendMarsTemperature...
+const sendMarsTemperature = (callback) => {
+  const temperatureCelsius = getMarsTemperature();
+  return callback(temperatureCelsius);
+}
 
 
-sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
-sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
+// imprime "It is currently 47ºF at Mars", por exemplo
+sendMarsTemperature(temperatureInFahrenheit);
+// imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
+sendMarsTemperature(greet);
