ad5f24bcaf612f2292e8693dcc06833e0d555437
LuanAlexandre <luanrb19@gmail.com>
2021-05-27 02:47:33 -0400

    Dia 9.2 - Promises: exercicio.5

--- exercises/Bloco_9/9.2/exercise.js
+++ exercises/Bloco_9/9.2/exercise.js
@@ -1,6 +1,4 @@
-const fetch = require('node-fetch');
-
-const fetchNumbers = new Promise((resolve, reject) => {
+const sumNumbers = new Promise((resolve, reject) => {
   const numbers = [];
   let sum = 0;
   for (let i = 0; i < 10; i += 1) {
@@ -11,5 +9,10 @@ const fetchNumbers = new Promise((resolve, reject) => {
   if (sum >= 8000) return reject(sum);
   resolve([sum/2, sum/3, sum/5, sum/10]);
 })
-.then(() => console.log(`Promise resolvida`))
+.then((divisions) => {
+  const sumOfDivisions = new Promise((resolve, reject) => {
+    const totalOfDivisions = divisions.reduce((acc, curr) => acc += curr, 0);
+    console.log(`Promise resolvida`);
+  })
+})
 .catch(() => console.log(`É mais de oito mil! Essa promise deve estar quebrada!`));
