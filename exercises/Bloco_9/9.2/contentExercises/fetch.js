1cb1df1becce2e2afd09a915d65737380bca2063
LuanAlexandre <luanrb19@gmail.com>
2021-05-27 00:47:43 -0400

    Dia 9.2 - Promises: adiciona codigos apresentados no conteudo

---
+++ exercises/Bloco_9/9.2/contentExercises/fetch.js
@@ -0,0 +1,27 @@
+/*
+O exemplo mostra que mesmo sendo resolvida imediatamente a solução não é exibida.
+*/
+
+const fetch = require('node-fetch');
+
+function verifiedFetch(url) {
+  return new Promise((resolve, reject) => {
+    if (url === 'https://api.chucknorris.io/jokes/random?category=dev') {
+      // fetch(url)
+      //   .then((r) => r.json())
+      //   .then((r) => resolve(r.value));
+      resolve('Sucessfull');
+    } else {
+      reject(new Error('endpoint não existe'));
+    }
+  });
+}
+
+function sendJokeToFriend(name) {
+  const message = verifiedFetch('https://api.chucknorris.io/jokes/random?category=dev')
+    .then((joke) => `Oi ${name}, ouve essa: ${joke}`)
+    .catch((err) => err);
+  console.log(message);
+}
+
+sendJokeToFriend("Anna");
