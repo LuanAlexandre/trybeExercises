7ecbf3efa68d3f9432d37418ef85fa63dc5d79e4
LuanAlexandre <luanrb19@gmail.com>
2021-05-27 02:21:40 -0400

    Dia 9.2 - Promises: exercicio.2

---
+++ exercises/Bloco_9/9.2/exercise.js
@@ -0,0 +1,15 @@
+const fetch = require('node-fetch');
+
+const fetchNumbers = new Promise((resolve, reject) => {
+  const numbers = [];
+  let sum = 0;
+  for (let i = 0; i < 10; i += 1) {
+    number = Math.floor(Math.random() * 51);
+    numbers.push(number*number);
+    sum += number*number;
+  }
+  if (sum >= 8000) return reject(sum);
+  resolve(sum);
+})
+.then(() => console.log(`Promise resolvida`))
+.catch(() => console.log(`Promise rejeitada`));
