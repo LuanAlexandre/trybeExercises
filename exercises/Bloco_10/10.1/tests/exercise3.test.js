b1e2c5d12b359a1bcefe22dc4aa15f1ed751bcea
LuanAlexandre <luanrb19@gmail.com>
2021-06-02 16:19:17 -0400

    Dia 10.1 - primeiros passos no jest: parte 1 - exercicio3

---
+++ exercises/Bloco_10/10.1/tests/exercise3.test.js
@@ -0,0 +1,21 @@
+const myRemoveWithoutCopy = require('../exercise3');
+
+describe('remover elemento de um array', () => {
+  it('Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) retorna o array esperado', () => {
+    expect(myRemoveWithoutCopy([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+  });
+
+  it('Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
+    expect(myRemoveWithoutCopy([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  });
+
+  const numbers = [2, 4, 6, 8, 10];
+  const newNumbers = myRemoveWithoutCopy(numbers, 4);
+  it('Faça uma chamada para a função myRemoveWithoutCopy e verifique se o array passado por parâmetro sofreu alterações', () => {
+    expect(newNumbers).toEqual(numbers);
+  });
+
+  it('Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 5) retorna o array esperado', () => {
+    expect(myRemoveWithoutCopy([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  });
+});
