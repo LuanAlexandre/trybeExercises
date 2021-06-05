4b87bd532d9b1604fea77f2ceb94a4c3fc2ff6fc
LuanAlexandre <luanrb19@gmail.com>
2021-06-05 15:00:32 -0400

    Dia 10.3 - simulando comportamentos: solucao do exercicio 2 de fixacao

--- exercises/Bloco_10/10.3/contentExercises.test.js
+++ exercises/Bloco_10/10.3/contentExercises.test.js
@@ -1,10 +1,18 @@
 const math = require('./contentExercises');
 
 describe('arithmetic operations', () => {
-  it('sum', () => {
+  it('call of minus', () => {
     math.subtrair = jest.fn();
     math.subtrair();
     expect(math.subtrair).toHaveBeenCalled();
+  });
+
+  it('multiply operation', () => {
+    math.multiplicar = jest.fn().mockReturnValue(10);
+    
+    math.multiplicar();
+    expect(math.multiplicar).toHaveBeenCalled();
+    expect(math.multiplicar(2, 5)).toBe(10);
   });
 });
 
