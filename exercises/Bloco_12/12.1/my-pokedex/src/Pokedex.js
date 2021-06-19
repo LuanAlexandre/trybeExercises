2f906ed5a3056153e1c533b769a633e546d4a563
LuanAlexandre <luanrb19@gmail.com>
2021-06-19 00:14:47 -0500

    Dia 12.1 - componentes com estados e eventos: adicao do botao que faz aparecer um pokemon por vez

---
+++ exercises/Bloco_12/12.1/my-pokedex/src/Pokedex.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Pokemon from './Pokemon';
+
+class Pokedex extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentPokemonIndex: 0,
+    }
+    this.changePokemon = this.changePokemon.bind(this);
+  }
+
+  changePokemon() {
+    if(this.state.currentPokemonIndex === this.props.pokemons.length - 1) {
+      this.setState(() => ({
+        currentPokemonIndex: 0,
+      }));
+    }
+    this.setState((previousState) => ({
+      currentPokemonIndex: previousState.currentPokemonIndex + 1,
+    }));
+  }
+
+  render() {
+    const { pokemons } = this.props;
+    const { currentPokemonIndex } = this.state;
+    return (
+      <div className="pokedex">
+        <Pokemon pokemon={ pokemons[currentPokemonIndex] } />
+        <button type="button" onClick={this.changePokemon}>Next Pokemon</button>
+      </div>
+    );
+  }
+}
+
+export default Pokedex;
