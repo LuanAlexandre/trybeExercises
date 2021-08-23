9cf685ab9eedb395c4b78396ef8df915f85a581b
LuanAlexandre <luanrb19@gmail.com>
2021-08-23 18:35:34 -0400

    Dia 16.1 - Introduçao ao Redux: solucao do exercicio 1.6

--- exercises/Bloco_16/16.1/script.js
+++ exercises/Bloco_16/16.1/script.js
@@ -16,9 +16,25 @@ window.onload = () => {
         ...state,
         index: state.index === 0 ? state.colors.length - 1 : state.index - 1,
       });
+    case 'RANDOM_COLOR':
+      return ({
+        ...state,
+        colors: [...state.colors, criarCor()],
+        index: state.colors.length - 1,
+      });
     default:
       return state;
     }
+  }
+
+  function criarCor() {
+    const oneChar = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+    let cor = '#';
+    const aleatorio = () => Math.floor(Math.random() * oneChar.length);
+    for (let i = 0; i < 6; i += 1) {
+        cor += oneChar[aleatorio()];
+    }
+    return cor;
   }
 
   const store = Redux.createStore(rootReducer);
@@ -32,6 +48,12 @@ window.onload = () => {
 
   buttonPreviousColor.addEventListener("click", () => {
     store.dispatch({ type: 'PREVIOUS_COLOR' });
+  });
+
+  const buttonRandomColor = document.querySelector("#random");
+
+  buttonRandomColor.addEventListener("click", () => {
+    store.dispatch({ type: 'RANDOM_COLOR' });
   });
 
   store.subscribe(() => {
