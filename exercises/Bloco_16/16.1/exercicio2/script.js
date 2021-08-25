f0a132fcf10b1e9000b1ce6edbe69ca22db7a5ee
LuanAlexandre <luanrb19@gmail.com>
2021-08-25 14:09:35 -0400

    Dia 16.1 - Introduçao ao Redux: solucao completa do exercicio 2

--- exercises/Bloco_16/16.1/exercicio2/script.js
+++ exercises/Bloco_16/16.1/exercicio2/script.js
@@ -27,3 +27,36 @@ const meuSegundoReducer = (state = ESTADO_INICIAL_2, action) => {
 };
 
 const rootReducer = Redux.combineReducers({ meuPrimeiroReducer, meuSegundoReducer});
+
+const store = Redux.createStore(rootReducer);
+
+const changeFirstName = (firstName) => ({
+  type: 'CHANGE_FIRST_NAME',
+  payload: firstName,
+});
+
+const changeLastName = (lastName) => ({
+  type: 'CHANGE_LAST_NAME',
+  payload: lastName,
+});
+
+window.onload = () => {
+  setTimeout(() => {
+    //Seu dispatch vem aqui //
+    const { meuPrimeiroReducer, meuSegundoReducer } = store.getState();
+    store.dispatch({ type: 'CHANGE_FIRST_NAME'});
+    store.dispatch({ type: 'CHANGE_LAST_NAME'});
+  }, 3000);
+}
+
+store.subscribe(() => {
+  const { meuPrimeiroReducer, meuSegundoReducer } = store.getState();
+  const nomeUm = document.getElementById('nome-1');
+  const sobrenomeUm = document.getElementById('sobrenome-1');
+  const nomeDois = document.getElementById('nome-2');
+  const sobrenomeDois = document.getElementById('sobrenome-2');
+  nomeUm.innerHTML = meuSegundoReducer.nome;
+  sobrenomeUm.innerHTML = meuSegundoReducer.sobrenome;
+  nomeDois.innerHTML = meuPrimeiroReducer.nome;
+  sobrenomeDois.innerHTML = meuPrimeiroReducer.sobrenome;
+});
