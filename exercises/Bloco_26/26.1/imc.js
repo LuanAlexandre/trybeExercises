78a29d7356dcc20d5d8e9cac853d7032489314cc
LuanAlexandre <luanrb19@gmail.com>
2021-12-01 19:25:28 -0400

    Exercicio 5: resolvido

--- exercises/Bloco_26/26.1/imc.js
+++ exercises/Bloco_26/26.1/imc.js
@@ -7,4 +7,11 @@ const imc = weight / (height * height);
 
 console.log(imc);
 
+if(imc < 18.5) console.log("Abaixo do peso (magreza)");
+if(imc >= 18.5 && imc <= 24.9) console.log("Peso normal");
+if(imc >= 25 && imc <= 29.9) console.log("Acima do peso (sobrepeso)");
+if(imc >= 30 && imc <= 34.9) console.log("Obesidade grau I");
+if(imc >= 35 && imc <= 39.9 ) console.log("Obesidade grau II");
+if(imc >= 40) console.log("Obesidade graus III e IV");
+
 module.exports = imc;
