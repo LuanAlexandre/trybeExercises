04c9233a50c31df72aed29b36306b4cbbaf9cd2f
LuanAlexandre <luanrb19@gmail.com>
2021-12-04 13:37:20 -0400

    Exercicio 4: requisito 3 resolvido

--- exercises/Bloco_26/26.2/scripts/dia26-2-04.js
+++ exercises/Bloco_26/26.2/scripts/dia26-2-04.js
@@ -22,3 +22,16 @@ async function findPersonById(personId) {
     console.log(`id não encontrado`);
   }
 }
+
+//Requisito 3
+async function filterPersons() {
+  try {
+    const resolve = await fs.readFile(simpsons, 'utf-8');
+    const content = await JSON.parse(resolve);
+    const filteredContent = content.filter(({ id }) => (id !== '6' && id !== '10'));
+    await fs.writeFile(simpsons, JSON.stringify(filteredContent));
+    console.log(`Filtragem realizada.`);
+  } catch(error) {
+    console.error(`Não foi possível escrever no arquivo: ${error}`);
+  }
+}
