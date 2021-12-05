321627914b25de341b9075899690bbca76b1fadc
LuanAlexandre <luanrb19@gmail.com>
2021-12-04 23:47:12 -0400

    Exercicio 2: resolvido

--- exercises/Bloco_26/26.4/index.js
+++ exercises/Bloco_26/26.4/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3001;
+
+app.use(bodyParser.json());
 
 //Exercício 1
 app.get('/ping', (req, res) => {
   res.status(200).json({ message: 'pong' });
+});
+
+//Exercício 2
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  res.status(201).json({ message: `Hello, ${name}` });
 });
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
