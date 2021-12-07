6b57d8e61767c802e8109f834341f722093ab8ca
LuanAlexandre <luanrb19@gmail.com>
2021-12-07 13:29:38 -0400

    Atividade 1: correcoes

--- exercises/Bloco_26/26.5/routes/atividade01.js
+++ exercises/Bloco_26/26.5/routes/atividade01.js
@@ -5,8 +5,8 @@ import authLoginMiddleware from '../middlewares/authLoginMiddleware.js';
 
 const router = express.Router();
 
-router.post('/user/register', rescue(authRegister));
+router.post('/register', rescue(authRegister));
 
-router.post('/user/login', rescue(authLoginMiddleware));
+router.post('/login', rescue(authLoginMiddleware));
 
 export default router;
