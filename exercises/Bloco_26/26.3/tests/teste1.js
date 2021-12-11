445c93f235bd89ed05ea8d4b580ce26623047c6a
LuanAlexandre <luanrb19@gmail.com>
2021-12-11 16:42:47 -0400

    Exercicio 3: resolvido

--- exercises/Bloco_26/26.3/tests/teste1.js
+++ exercises/Bloco_26/26.3/tests/teste1.js
@@ -1,25 +1,57 @@
 import { expect } from "chai";
 import avaliateNumber from '../avaliateNumber.js';
 
-describe('Avalia a natureza de um número', () => {
-  describe('se maior que 0 retorna "positivo"', () => {
-    it('positivo', () => {
-      const answer = avaliateNumber(4);
-      expect(answer).to.be.equals('positivo');
+describe('Avalia a natureza de um valor', () => {
+  describe('se o valor é um número e', () => {
+    describe('é maior que 0 retorna', () => {
+      it('"positivo"', () => {
+        const answer = avaliateNumber(4);
+        expect(answer).to.be.equals('positivo');
+      });
+    });
+
+    describe('é menor que 0 retorna', () => {
+      it('"negativo"', () => {
+        const answer = avaliateNumber(-3);
+        expect(answer).to.be.equals('negativo');
+      });
+    });
+
+    describe('é igual a 0 retorna', () => {
+      it('"neutro"', () => {
+        const answer = avaliateNumber(0);
+        expect(answer).to.be.equals('neutro');
+      });
     });
   });
 
-  describe('se menor que 0 retorna "negativo"', () => {
-    it('negativo', () => {
-      const answer = avaliateNumber(-3);
-      expect(answer).to.be.equals('negativo');
+  describe('se não for um número e', () => {
+    describe('é um objeto retorna', () => {
+      it('"o valor deve ser um número"', () => {
+        const answer = avaliateNumber({});
+        expect(answer).to.be.equals('o valor deve ser um número');
+      });
     });
-  });
 
-  describe('se igual a 0 retorna "neutro"', () => {
-    it('neutro', () => {
-      const answer = avaliateNumber(0);
-      expect(answer).to.be.equals('neutro');
+    describe('é um array retorna', () => {
+      it('"o valor deve ser um número"', () => {
+        const answer = avaliateNumber([]);
+        expect(answer).to.be.equals('o valor deve ser um número');
+      });
+    });
+
+    describe('é uma string retorna', () => {
+      it('"o valor deve ser um número"', () => {
+        const answer = avaliateNumber('Olá');
+        expect(answer).to.be.equals('o valor deve ser um número');
+      });
+    });
+
+    describe('é um booleano retorna', () => {
+      it('"o valor deve ser um número"', () => {
+        const answer = avaliateNumber(true);
+        expect(answer).to.be.equals('o valor deve ser um número');
+      });
     });
   });
 });
