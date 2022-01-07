7e33b530b81feff75015e0859b915bb76b1f8362
Luan <luanrb19@gmail.com>
2022-01-07 19:57:30 -0400

    Exercício 4

--- exercises/Bloco_27/27.3/controllers/productController.js
+++ exercises/Bloco_27/27.3/controllers/productController.js
@@ -6,13 +6,13 @@ const router = express.Router();
 router.get('/list-beers', async (req, res, next) => {
   const products = await ProductModel.getAll();
 
-  return res.json(products);
+  return res.status(200).json(products);
 });
 
 router.get('/get-by-id/:id', async (req, res, next) => {
   const product = await ProductModel.getById(req.params.id);
 
-  return res.json(product);
+  return res.status(200).json(product);
 });
 
 router.post('/add-beer', async (req, res) => {
@@ -20,13 +20,13 @@ router.post('/add-beer', async (req, res) => {
 
   const newProduct = await ProductModel.add(name, brand);
 
-  return res.json(newProduct);
+  return res.status(201).json(newProduct);
 });
 
 router.delete('/delete-beer/:id', async (req, res) => {
   const products = await ProductModel.exclude(req.params.id);
 
-  return res.json(products);
+  return res.status(200).json(products);
 });
 
 router.put('/update-beer/:id', async (req, res) => {
@@ -34,7 +34,7 @@ router.put('/update-beer/:id', async (req, res) => {
 
   const products = await ProductModel.update(req.params.id, name, brand);
 
-  return res.json(products);
+  return res.status(200).json(products);
 });
 
 module.exports = router;
