4d56d32c67478156fa64dc86d3888de51c48c383
Luan <luanrb19@gmail.com>
2022-01-07 02:11:00 -0400

    Refatoracao do codigo

--- exercises/Bloco_27/27.2/controllers/cepController.js
+++ exercises/Bloco_27/27.2/controllers/cepController.js
@@ -1,50 +1,30 @@
-const Joi = require('joi');
-const { validateCep, findCep, createCep } = require('../services/cepService');
+const { findCep, createCep } = require('../services/cepService');
 
-const find = async (req, res, next) => {
+const find = async (req, res, _next) => {
   try {
     const { cep } = req.params;
-    const isValid = validateCep(cep);
-
-    if (!isValid) {
-      return res.status(400).json({ error: { code: 'invalidData', message: 'CEP inválido' } });
-    }
 
     const result = await findCep(cep);
 
-    if (!result) {
-      return res.status(404).json({ error: { code: 'notFound', message: 'CEP não encontrado' } });
-    }
-
     return res.status(200).json(result);
+
   } catch (error) {
-    console.error(error.message);
-    return next(error);
+    console.error(`GET FIND CEP -> ${error.error.message}`);
+    return res.status(error.error.status).json(error.error.message);
   }
 };
 
-const create = async (req, res, next) => {
-  const info = Joi.object({
-    cep: Joi.string().not().empty().required(),
-    logradouro: Joi.string().not().empty().required(),
-    bairro: Joi.string().not().empty().required(),
-    localidade: Joi.string().not().empty().required(),
-    uf: Joi.string().not().empty().required(),
-  });
+const create = async (req, res, _next) => {
+  try {
+    const { body } = req;
 
-  const isValid = info.validate(req.body);
+    const newCep = await createCep(body);
 
-  if (isValid.error) return next(isValid.error);
-
-  const { cep, logradouro, bairro, localidade, uf } = req.body;
-
-  const newCep = await createCep(cep, logradouro, bairro, localidade, uf);
-
-  if (newCep.error) return next(newCep.error);
-
-  if (newCep) return next({ error: { code: 'alreadyExists', message: 'CEP já existente' }});
-
-  return res.status(201).json(newCep);
+    return res.status(201).json(newCep);
+  } catch (error) {
+    console.error(`POST CREATE CEP -> ${error.error.message}`);
+    return res.status(error.error.status).json(error.error.message);
+  }
 };
 
 module.exports = {
--- exercises/Bloco_27/27.2/services/cepService.js
+++ exercises/Bloco_27/27.2/services/cepService.js
@@ -1,24 +1,53 @@
+const Joi = require('joi');
 const cepModel = require('../models/cepModel');
+const apiModel = require('../models/externalApiModel');
+
+const cepSchema = Joi.object({
+  cep: Joi.string().not().empty().required(),
+  logradouro: Joi.string().not().empty().required(),
+  bairro: Joi.string().not().empty().required(),
+  localidade: Joi.string().not().empty().required(),
+  uf: Joi.string().not().empty().required(),
+});
 
 const validateCep = (cep) => {
   const validCep = /\d{5}-?\d{3}/;
   return validCep.test(cep);
-}
+};
+
+const validateInfo = (info) => {
+  const { error } = cepSchema.validate(info);
+
+  if (error) throw { error: { code: 'invalidData', status: 400, message: error.message } };
+
+  return info;
+};
 
 const findCep = async (cep) => {
+  const isValidCep = validateCep(cep);
+
+  if (!isValidCep) throw { error: { code: 'invalidData', status: 400, message: 'CEP inválido' }};
+
   const answer = await cepModel.searchCep(cep);
+
+  if (!answer) throw { error: { code: 'notFound', status: 404, message: 'CEP não encontrado'}};
 
   return answer;
 };
 
-const createCep = async (cep, logradouro, bairro, localidade, uf) => {
+const createCep = async ({ cep, logradouro, bairro, localidade, uf }) => {
+  validateInfo({ cep, logradouro, bairro, localidade, uf });
+
+  const exists = await findCep(cep);
+
+  if (exists && exists.length !== 0) throw { error: { code: 'alreadyExists', status: 409, message: 'CEP já existente' }};
+
   const answer = await cepModel.createCep(cep, logradouro, bairro, localidade, uf);
 
   return answer;
-}
+};
 
 module.exports = {
-  validateCep,
   findCep,
   createCep,
 };
