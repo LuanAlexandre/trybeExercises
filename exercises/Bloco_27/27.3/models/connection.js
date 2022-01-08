b50b6725ad2c710e0631e68bd98bea6aff8235d8
Luan <luanrb19@gmail.com>
2022-01-07 20:07:20 -0400

    Alteracoes na conexao com o banco para usar dotenv

--- exercises/Bloco_27/27.3/models/connection.js
+++ exercises/Bloco_27/27.3/models/connection.js
@@ -2,10 +2,14 @@ const mysql = require('mysql2/promise');
 
 require('dotenv').config();
 
+const HOST = process.env.DB_HOST || 'localhost';
+const USER = process.env.DB_USER || 'root';
+const PASSWORD = process.env.DB_PASS;
+
 const connection = mysql.createPool({
-  host: process.env.HOST, // Se necessário, substitua pelo seu host, `localhost` é o comum
-  user: process.env.USER, // Se necessário, substitua pelo seu usuário para conectar ao banco na sua máquina
-  password: process.env.PASSWORD, // Se necessário, substitua pela sua senha para conectar ao banco na sua máquina
+  host: HOST, // Se necessário, substitua pelo seu host, `localhost` é o comum
+  user: USER, // Se necessário, substitua pelo seu usuário para conectar ao banco na sua máquina
+  password: PASSWORD, // Se necessário, substitua pela sua senha para conectar ao banco na sua máquina
   database: 'rest_exercicios',
 });
 
